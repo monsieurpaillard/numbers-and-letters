761a1269716ffafaa67e5491313f9236e1b81553
Romain Paillard <romain.paillard@gmail.com>
2015-10-28 17:04:52 +0100

    missing in grid

--- app/controllers/party_controller.rb
+++ app/controllers/party_controller.rb
@@ -1,12 +1,17 @@
 require 'scripts/game_methods'
 
 class PartyController < ApplicationController
+
   def game
     @grid = generate_grid(9).join
   end
 
   def score
-    @word = params[:word]
+    @word        = params[:word]
+    @start_time  = Time.parse(params[:start_time])
+    @translation = find_on_wordreference(@word)
+    @end_time    = Time.now
+    @time        = @end_time - @start_time
   end
 end
 
--- lib/scripts/game_methods.rb
+++ lib/scripts/game_methods.rb
@@ -1,4 +1,6 @@
-# Ici toutes les méthodes liées à la logique du jeu
+require 'open-uri'
+require 'json'
+
 def generate_grid(grid_size)
   array = []
   for i in (1..grid_size)
@@ -7,32 +9,21 @@ def generate_grid(grid_size)
   array
 end
 
-def run_game(attempt, grid, start_time, end_time)
-  result = {}
-  result[:time] = end_time - start_time
-  result[:translation] = translate(attempt)
-  result[:score] = score(attempt, start_time, end_time)
-  result[:message] = translate(attempt)
-  result
-end
-
-def score(attempt, start_time, end_time)
-  a = attempt.length + 100
-  b = end_time - start_time + 100
-  c = a + b
-end
-
-
-def translate(word)
-  api_url = "http://api.wordreference.com/0.8/80143/json/enfr/#{word}"
-  open(api_url) do |stream|
-    translation = JSON.parse(stream.read)
-    if translation["Error"] == "NoTranslation"
-      return nil
-    elsif translation["Response"] == "Redirect"
-      return "not an english word"
-    else
-    translation['term0']['PrincipalTranslations']['0']['FirstTranslation']['term']
+def find_on_wordreference(word)
+    api_url = "http://api.wordreference.com/0.8/80143/json/enfr/#{word}"
+    open(api_url) do |stream|
+      translation = JSON.parse(stream.read)
+      if translation["Error"] == "NoTranslation"
+        return nil
+      elsif translation["Response"] == "Redirect"
+        return "not an english word"
+      else
+        return translation['term0']['PrincipalTranslations']['0']['FirstTranslation']['term']
     end
   end
+end
+
+def in_grid?(word,grid)
+  grid = generate_grid(grid)
+
 end
